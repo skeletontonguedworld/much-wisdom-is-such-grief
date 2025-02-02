962c2095033cb7c8da29eb3c718bc151e82b5b81
skeletontonguedworld <129410383+skeletontonguedworld@users.noreply.github.com>
2025-02-02 15:14:10 +0100

    drowned
    Commituję ino po to, żeby łatwiej było cofnąć, jak naspierdalam XD

--- windwalker.nim
+++ windwalker.nim
@@ -1,7 +1,7 @@
 import std/random
 randomize()
-
 import os
+import strutils
 
 var gracz_hp = 100
 
@@ -24,17 +24,19 @@ proc przegrana (lsr: string = "OJOJOJ, NIE UDAŁO SIEM WYGRAĆ Z NASZYM SQUISHY 
 proc wynik (rsl: string = "A OTO WYNIK NASZEJ WALKI, ZIOMECZKU"): string =
   while gracz_hp > 0:
 
-    if gracz_hp > int Pauweł or gracz_hp > int Pauweł_2 or gracz_hp > int Kitek or gracz_hp > int Relaxo or gracz_hp > int Bałszkitka: #tutaj wiem, o co chodzi, ale nie umiem tego rozwiązać
+    if gracz_hp.ord > Pauweł.ord or gracz_hp.ord > Pauweł_2.ord or gracz_hp.ord > Kitek.ord or gracz_hp.ord > Relaxo.ord or gracz_hp.ord > Bałszkitka.ord:
       echo wygrana()
       break
-    elif gracz_hp < int Kitek:
+    elif gracz_hp < Pauweł.ord or gracz_hp.ord < Pauweł_2.ord or gracz_hp.ord < Kitek.ord or gracz_hp.ord < Relaxo.ord or gracz_hp.ord < Bałszkitka.ord:
       echo przegrana()
       break
 
-proc losowanie (los: string = "Czas na losowanie pierwszego przeciwnika"): string =
+proc losowanie (los: string = "Czas na losowanie przeciwnika"): string =
   shuffle(squishy_squad)
   var przeciwnik = sample(squishy_squad)
-  echo przeciwnik
+  echo przeciwnik.ord
+
+var przeciwnikfinal = losowanie()
 
 while gracz_hp > 0:
   
@@ -46,7 +48,23 @@ while gracz_hp > 0:
   sleep 1000
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   sleep 1000
-  echo losowanie()
+  echo "Czy kcesz wziąć w tym udział?"
+  sleep 1000
+
+  var gracz = readLine(stdin)
+
+  if gracz.contains ("ak"):
+    echo "Czas na losowanie pierwszego przeciwnika!"
+    sleep 1000
+    echo "Twój przeciwnik to:"
+    shuffle(squishy_squad)
+    var przeciwnik = sample(squishy_squad)
+    echo przeciwnik
+    echo "Zaczynajmy!"
+    gracz_hp -= rand(1..150)
+    przeciwnik.ord -= rand(1..150)
+
+
   
 
   break
