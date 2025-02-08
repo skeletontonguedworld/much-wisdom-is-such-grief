5c997363b57de35d095cf12c46d12c2e337fb86b
skeletontonguedworld <129410383+skeletontonguedworld@users.noreply.github.com>
2025-02-08 17:02:55 +0100

    distortion wind
    nadal je to chujnia, ale udało się osiągnąć to, co kciałam, może jeszcze postaram się to rozwinąć i ulepszyć, ale nie wiem

--- windwalker.nim
+++ windwalker.nim
@@ -3,15 +3,16 @@ randomize()
 import os
 import strutils
 
-var gracz_hp = 100
+var gracz_hp = 200
+var przeciwnik_hp = 0
 
-var pauweł = (a: 80, b: "Pauweł")
-var pauweł_2 = (a: 90, b: "Pauweł 2")
-var kitek = (a: 120, b: "Kitek")
-var relaxo = (a: 125, b: "Relaxo")
-var bałszkitka = (a: 150, b: "Bałszkitka")
+var pauweł = "Pauweł"
+var pauweł_2 = "Pauweł 2"
+var kitek = "Kitek"
+var relaxo = "Relaxo"
+var bałszkitka = "Bałszkitka"
 
-var squishy_squad = [pauweł.b, pauweł_2.b, kitek.b, relaxo.b, bałszkitka.b] 
+var squishy_squad = [pauweł, pauweł_2, kitek, relaxo, bałszkitka] 
 
 proc wygrana (win: string = "WYGRAŁEŚ MORDO!"): string =
   echo $win
@@ -22,10 +23,10 @@ proc przegrana (lsr: string = "OJOJOJ, NIE UDAŁO SIEM WYGRAĆ Z NASZYM SQUISHY 
 proc wynik (rsl: string = "A OTO WYNIK NASZEJ WALKI, ZIOMECZKU"): string =
   while gracz_hp > 0:
 
-    if gracz_hp > pauweł.a or gracz_hp > pauweł_2.a or gracz_hp > kitek.a or gracz_hp > relaxo.a or gracz_hp > bałszkitka.a:
+    if gracz_hp > przeciwnik_hp:
       echo wygrana()
       break
-    elif gracz_hp < pauweł.a or gracz_hp < pauweł_2.a or gracz_hp < kitek.a or gracz_hp < relaxo.a or gracz_hp < bałszkitka.a:
+    elif gracz_hp < przeciwnik_hp:
       echo przegrana()
       break
 
@@ -51,16 +52,88 @@ while gracz_hp > 0:
     shuffle(squishy_squad)
     var przeciwnik = sample(squishy_squad)
     echo przeciwnik
-    echo "Zaczynajmy!"
-    gracz_hp -= rand(1..150)
-    #przeciwnik -= rand(1..150) #nie, koniec na dzisiaj, nadal tu jest problem i nie wiem jak połączyć nazwę ze zdrowiem u random przeciwnika 
+
+    if przeciwnik == "Pauweł":
+      przeciwnik_hp = 80
+    elif przeciwnik == "Pauweł 2":
+      przeciwnik_hp = 90
+    elif przeciwnik == "Kitek":
+      przeciwnik_hp = 100
+    elif przeciwnik == "Relaxo":
+      przeciwnik_hp = 125
+    elif przeciwnik == "Bałszkitka":
+      przeciwnik_hp = 150
     
+    echo "Zaczynajmy!"
+
+    gracz_hp -= rand(1..50)
+    przeciwnik_hp -= rand(1..150)
     
     sleep 2000
-    echo "Po ciężkiej walce Twoje zdrówko to " & $gracz_hp & " a zdróweczko Twojego przeciwnika to " & $przeciwnik
+    echo "Po ciężkiej walce Twoje zdrówko to " & $gracz_hp & " a zdróweczko Twojego przeciwnika to " & $przeciwnik_hp
     sleep 2000
     echo wynik()
 
-  
+    echo "Czas na drugiego przeciwnika z naszego Squishy Squadu"
+    sleep 1000
+    echo "Twój przeciwnik to:"
+    shuffle(squishy_squad)
+    var przeciwnik2 = sample(squishy_squad)
+    echo przeciwnik2
 
-  break
+    if przeciwnik2 == "Pauweł":
+      przeciwnik_hp = 80
+    elif przeciwnik2 == "Pauweł 2":
+      przeciwnik_hp = 90
+    elif przeciwnik2 == "Kitek":
+      przeciwnik_hp = 100
+    elif przeciwnik2 == "Relaxo":
+      przeciwnik_hp = 125
+    elif przeciwnik2 == "Bałszkitka":
+      przeciwnik_hp = 150
+    
+    echo "Zaczynajmy!"
+
+    gracz_hp -= rand(1..100)
+    przeciwnik_hp -= rand(1..150)
+    
+    sleep 2000
+    echo "Po ciężkiej walce Twoje zdrówko to " & $gracz_hp & " a zdróweczko Twojego przeciwnika to " & $przeciwnik_hp
+    sleep 2000
+    echo wynik()
+
+    echo "Czas na trzecią walkę, tym razem zmierzysz się z:"
+    shuffle(squishy_squad)
+    var przeciwnik3 = sample(squishy_squad)
+    echo przeciwnik3
+
+    if przeciwnik3 == "Pauweł":
+      przeciwnik_hp = 80
+    elif przeciwnik3 == "Pauweł 2":
+      przeciwnik_hp = 90
+    elif przeciwnik3 == "Kitek":
+      przeciwnik_hp = 100
+    elif przeciwnik3 == "Relaxo":
+      przeciwnik_hp = 125
+    elif przeciwnik3 == "Bałszkitka":
+      przeciwnik_hp = 150
+    
+    echo "Zaczynajmy!"
+
+    gracz_hp -= rand(1..150)
+    przeciwnik_hp -= rand(1..150)
+    
+    sleep 2000
+    echo "Po ciężkiej walce Twoje zdrówko to " & $gracz_hp & " a zdróweczko Twojego przeciwnika to " & $przeciwnik_hp
+    sleep 2000
+    echo wynik()
+
+    if gracz_hp > 0:
+     echo "No to se chyba ogółem wygrałeś, grats"
+     break
+    else:
+      echo "No nie udało się mordo wygrać całego Squish MMA, ale próbuj"
+  
+  else: 
+    echo "No to nie wiem, po coś tu przylizł, żegnam"
+    break
